ef5d911d864730251964c6e186f5d8fcda473a4e
GenesisMauries <exodo_54@hotmail.com>
2018-08-13 20:17:39 -0500

    intento de filtrado

--- src/js/admin.js
+++ src/js/admin.js
@@ -32,23 +32,45 @@ const templateVisitors = db.collection('newVisitor').onSnapshot((querySnapshot) 
   });
 });
 
-// funcion para filtrar libros
-const searchVisitors = document.forms['search-visitor'].querySelector('input');
 
-searchVisitors.addEventListener('keyup', function(e) {
-  const term = e.target.value.toLowerCase();
-  const visitors = printVisitorsTable.getElementsByTagName('tr');
-  Array.from(visitors).forEach(function(visitor) {
-    const certainVisitor = visitors.textContent;
-    if (certainVisitor.toLowerCase().indexOf(term) != -1) {
-      certainVisitor.style.display = 'block';
-    } else {
-      certainVisitor.style.display = 'none';
+const filterTable = () =>{
+  const myInput = document.getElementById('text');
+  const filterTwo = myInput.value.toUpperCase();
+  const myTable = document.getElementById('myTable');
+  const tr = myTable.getElementsByTagName('tr');
+  for (let i = 0; i < tr.length; i++) {
+    let td = tr[i].getElementsByTagName('td')[0];
+    if (td) {
+      if (td.innerHTML.toLocaleUpperCase().indexOf(filter) > -1) {
+        tr [i].style.display = '';
+      } else {
+        tr[i].style.display = 'none';
+      }
     }
-  });
-});
+  }
+};
 
-// const listeneres = (array) => {
+const send = document.getElementById('send');
+
+send.addEventListener('click', filterTable);
+
+// // funcion para filtrar 
+// const searchVisitors = document.forms['search-visitor'].querySelector('input');
+
+// searchVisitors.addEventListener('keyup', function(e) {
+//   const term = e.target.value.toLowerCase();
+//   const visitors = printVisitorsTable.getElementsByTagName('tr');
+//   Array.from(doc.data()).forEach(function(visitor) {
+//     const certainVisitor = visitors.textContent;
+//     if (certainVisitor.toLowerCase().indexOf(term) != -1) {
+//       certainVisitor.style.display = 'block';
+//     } else {
+//       certainVisitor.style.display = 'none';
+//     }
+//   });
+// });
+
+// // const listeneres = (array) => {
 //   search.addEventListener('keyup', () => {
 //       if (search.value.trim().length > 0) {
 //           const searching = search.value.toLowerCase();
@@ -60,4 +82,3 @@ searchVisitors.addEventListener('keyup', function(e) {
 //       }
 //   });
 // }
-
